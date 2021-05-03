265d57eb6dca644ba659de3534e06a0873a317f9
HenriqueOmena <88lmao@gmail.com>
2021-05-03 22:47:14 +0100

    Setuping my tests first - TDD

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { shallow } from "enzyme";
+import PersonList from "./PersonaList";
 // test('renders learn react link', () => {
 //   render(<App />);
 //   const linkElement = screen.getByText(/learn react/i);
@@ -10,5 +11,12 @@ import { shallow } from "enzyme";
 describe("App", () => {
   it("Some title", () => {
     const appWrapper = shallow(<App />);
+  });
+
+  it("render Person list", () => {
+    const appWrapper = shallow(<App />);
+    const personList = appWrapper.find(PersonList);
+
+    expect(personList).toHaveLength(1);
   });
 });
---
+++ src/PersonaList.js
@@ -0,0 +1,3 @@
+const PersonList = () => {};
+
+export default PersonList;

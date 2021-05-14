9e0c25a70cce1b34fae84ef42d35eef50973d093
HenriqueOmena <88lmao@gmail.com>
2021-05-14 23:01:55 +0100

    Done Reducers slices

--- src/redux-toolkit/index.ts
+++ src/redux-toolkit/index.ts
@@ -20,13 +20,55 @@ const todosInitialState: Todo[] = [
   },
 ];
 
-const todoSlice = createSlice({
+const todosSlice = createSlice({
   name: "todos",
   initialState: todosInitialState,
   reducers: {
+    create: {
+      reducer: (
+        state,
+        { payload }: PayloadAction<{ id: string; isComplete: boolean; desc: string }>
+      ) => {
+        state.push(payload);
+      },
+      prepare: ({ desc }: { desc: string }) => ({
+        payload: {
+          id: uuid(),
+          desc,
+          isComplete: false,
+        },
+      }),
+    },
     edit: (state, { payload }: PayloadAction<{ id: string; desc: string }>) => {
       const todoToEdit = state.find((todo) => todo.id === payload.id);
-      if (todoToEdit) todoToEdit.desc = payload.desc;
+      if (todoToEdit) todoToEdit.desc = payload.desc; // we dont need to
     },
+    toggle: (state, { payload }: PayloadAction<{ id: string; isComplete: boolean }>) => {
+      const todoToggle = state.find((todo) => todo.id === payload.id);
+      if (todoToggle) todoToggle.isComplete = payload.isComplete; // we dont need to
+    },
+    remove: (state, { payload }: PayloadAction<{ id: string }>) => {
+      const deleteTodo = state.filter((todo) => todo.id !== payload.id);
+    },
+  },
+});
+
+const selectedTodoSlice = createSlice({
+  name: "selectedTodo",
+  initialState: null as string | null,
+  reducers: {
+    select: (state, { payload }: PayloadAction<{ id: string }>) => payload.id,
+  },
+});
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: 0,
+  reducers: {},
+  extraReducers: {
+    [todosSlice.actions.create.type]: (state) => state + 1,
+    [todosSlice.actions.edit.type]: (state) => state + 1,
+    [todosSlice.actions.toggle.type]: (state) => state + 1,
+    [todosSlice.actions.remove.type]: (state) => state + 1,
   },
 });
